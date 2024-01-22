7832c4befcb3b4cf61e1e7dd724e428793d0c3ff
mukul-kr <mukul0000kumar@gmail.com>
2024-01-22 15:07:39 +0530

    init  1

--- app.py
+++ app.py
@@ -3,20 +3,32 @@ from graphviz import Digraph
 class TreeNode:
     def __init__(self, digit):
         self.digit = digit
-        self.children = []
+        self.children = [] 
 
 class BinaryTree:
     def __init__(self):
         self.root = None
 
-    def insert(self, digit):
-        self.root = self._insert(self.root, digit)
+    # def insert(self, digit):
+    #     self.root = self._insert(self.root, digit)
+
+
+    def insert_digit_by_list(self, digit_list):
+        temp = TreeNode(digit_list[0])
+        self.root = temp
+        #print(temp)
+        for i in range(1, len(digit_list)):
+            #print(digit_list[i])
+            temp = self._insert(temp,digit_list[i])
 
     def _insert(self, node, digit):
         if node is None:
             return TreeNode(digit)
-        node.children.append(TreeNode(digit))
-        return node
+        newchild = TreeNode(digit)
+        #print(newchild)
+        #print(f"node: {node}")
+        node.children.append(newchild)
+        return newchild
 
     def search(self, digit):
         return self._search(self.root, digit)
@@ -36,13 +48,19 @@ class HashMapTree:
     def insert_empty_tree(self, key):
         self.hashmap[key] = BinaryTree()
 
+
     def insert_digit(self, key, number):
         if key in self.hashmap:
-            tree = self.hashmap[key]
-            for digit in str(number):
-                tree.insert(int(digit))
+            tree: BinaryTree = self.hashmap[key]
+            # #####################
+            # for digit in str(number):
+            #     tree.insert(int(digit))
+
+            list_of_digits = [int(digit) for digit in str(number)]
+            tree.insert_digit_by_list(list_of_digits)
         else:
             print(f"Tree not found for key {key}")
+
 
     def search_digit(self, key, digit):
         if key in self.hashmap:
@@ -79,7 +97,17 @@ hashmap_tree.insert_empty_tree("1")
 
 # Insert digits into trees
 # hashmap_tree.insert_digit("0", 1234761234)
-hashmap_tree.insert_digit("0", 789789)
-
-# Visualize the hashmap
+hashmap_tree.insert_digit("0", 456789)
+hashmap_tree.insert_digit("1", 456789)
 hashmap_tree.visualize()
+
+# for i,j in hashmap_tree.hashmap.items():
+#     if j.root is not None:
+#         print(j.root.digit)
+#         child = j.root.children[0]
+#         for i in range(6):
+#             print(child.digit)
+#             if len(child.children) == 0:
+#                 break
+#             child = child.children[0]
+#     print("")
