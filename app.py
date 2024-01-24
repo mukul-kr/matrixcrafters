8ff12bbcde4cb7f597a8637cc92e6ed74f3b5e7e
mukul-kr <mukul0000kumar@gmail.com>
2024-01-24 13:38:59 +0530

    Working

--- app.py
+++ app.py
@@ -1,45 +1,53 @@
 from graphviz import Digraph
+import random
+import string
+import time
+import threading
 
 class TreeNode:
     def __init__(self, digit):
         self.digit = digit
-        self.children = [] 
+        self.children = set()
 
 class BinaryTree:
     def __init__(self):
         self.root = None
 
-    # def insert(self, digit):
-    #     self.root = self._insert(self.root, digit)
-
-
     def insert_digit_by_list(self, digit_list):
-        temp = TreeNode(digit_list[0])
-        self.root = temp
-        #print(temp)
-        for i in range(1, len(digit_list)):
-            #print(digit_list[i])
-            temp = self._insert(temp,digit_list[i])
+        self._insert(self.root, digit_list, 0)
 
-    def _insert(self, node, digit):
-        if node is None:
-            return TreeNode(digit)
-        newchild = TreeNode(digit)
-        #print(newchild)
-        #print(f"node: {node}")
-        node.children.append(newchild)
-        return newchild
-
-    def search(self, digit):
-        return self._search(self.root, digit)
-
-    def _search(self, node, digit):
-        if node is None or node.digit == digit:
+    def _insert(self, node, digit_list, idx=0):
+        if idx >= len(digit_list):
             return node
-        for child in node.children:
-            if child.digit == digit:
-                return child
-        return None
+        if node is not None:
+            for i in node.children:
+                if i.digit == digit_list[idx]:
+                    return self._insert(i, digit_list, idx + 1)
+        # check if idx 0 is digit which is already present as value of root
+        if idx == 0 and node is not None and node.digit == digit_list[idx]:
+            return self._insert(node, digit_list, idx + 1)
+        if node is None:
+            newchild = TreeNode(digit_list[idx])
+            self.root = newchild
+        else:
+            newchild = TreeNode(digit_list[idx])
+            # node.children.append(newchild)
+            node.children.add(newchild)
+        return self._insert(newchild, digit_list, idx + 1)
+
+    def search(self, digit_list):
+        return self._search(self.root, digit_list)
+
+    def _search(self, node, digit_list, idx=0):
+        if idx >= len(digit_list) or node is None:
+            return False
+        if node.digit == digit_list[idx]:
+            if idx == len(digit_list) - 1:
+                return True
+            for child in node.children:
+                if self._search(child, digit_list, idx + 1):
+                    return True
+        return False
 
 class HashMapTree:
     def __init__(self):
@@ -49,26 +57,43 @@ class HashMapTree:
         self.hashmap[key] = BinaryTree()
 
 
+
     def insert_digit(self, key, number):
         if key in self.hashmap:
             tree: BinaryTree = self.hashmap[key]
-            # #####################
-            # for digit in str(number):
-            #     tree.insert(int(digit))
-
             list_of_digits = [int(digit) for digit in str(number)]
             tree.insert_digit_by_list(list_of_digits)
         else:
             print(f"Tree not found for key {key}")
 
-
-    def search_digit(self, key, digit):
+    def search_digit(self, key, number):
         if key in self.hashmap:
-            node = self.hashmap[key].search(digit)
+            node = self.hashmap[key].search([int(digit) for digit in str(number)])
             return node is not None
         else:
             print(f"Tree not found for key {key}")
             return False
+
+
+
+    def visualize_tree(self, key):
+        # Check if key exists in the hashmap
+        if key in self.hashmap:
+            tree = self.hashmap[key]
+            # Create a Graphviz object for visualization
+            dot = Digraph(comment=key)
+            # Generate DOT language script for the tree
+            self._generate_dot_script(tree.root, dot, "")
+            # Save and render the visualization
+            filename = f"{key}_tree"
+            dot.render(filename, format="png", cleanup=True)
+
+    def _generate_dot_script(self, node, dot, parent_key):
+        if node:
+            dot.node(f"{parent_key}_{node.digit}", str(node.digit))
+            for child in node.children:
+                dot.edge(f"{parent_key}_{node.digit}", f"{parent_key}_{child.digit}", label='Child')
+                self._visualize_tree(dot, child, parent_key)
 
     def visualize(self):
         dot = Digraph(comment='HashMapTree')
@@ -86,28 +111,86 @@ class HashMapTree:
                 dot.edge(f"{parent_key}_{node.digit}", f"{parent_key}_{child.digit}", label='Child')
                 self._visualize_tree(dot, child, parent_key)
 
+    def print_all_trees(self):
+        for key, tree in self.hashmap.items():
+            print(f"Key: {key}")
+            print("Tree:")
+            self._print_tree(tree.root, 0)
+            print("\n")
+
+    def _print_tree(self, node, depth):
+        if node is not None:
+            print(" " * depth + str(node.digit))
+            for child in node.children:
+                self._print_tree(child, depth + 1)
+
+
+def insert_digit_wrapper(hashmap_tree, key, random_numbers):
+    for number in random_numbers:
+            hashmap_tree.insert_digit(key, number)
+
+
+def generate_random_numbers(start, end, num):
+    return [random.randint(start, end) for _ in range(num)]
+
+
+
+
 # Example usage:
-hashmap_tree = HashMapTree()
 
 # Insert empty trees
-hashmap_tree.insert_empty_tree("0")
-hashmap_tree.insert_empty_tree("1")
-# hashmap_tree.insert_empty_tree("2")
-# hashmap_tree.insert_empty_tree("3")
+# hashmap_tree.insert_empty_tree("0")
+# hashmap_tree.insert_empty_tree("1")
 
-# Insert digits into trees
-# hashmap_tree.insert_digit("0", 1234761234)
-hashmap_tree.insert_digit("0", 456789)
-hashmap_tree.insert_digit("1", 456789)
-hashmap_tree.visualize()
+# # Insert digits into trees
+# hashmap_tree.insert_digit("0", 456789)
+# hashmap_tree.insert_digit("0", 452749)
+# hashmap_tree.insert_digit("0", 452741)
+# hashmap_tree.insert_digit("0", 466789)
+# hashmap_tree.insert_digit("1", 456789)
+# hashmap_tree.insert_digit("1", 452789)
 
-# for i,j in hashmap_tree.hashmap.items():
-#     if j.root is not None:
-#         print(j.root.digit)
-#         child = j.root.children[0]
-#         for i in range(6):
-#             print(child.digit)
-#             if len(child.children) == 0:
-#                 break
-#             child = child.children[0]
-#     print("")
+
+# hashmap_tree.print_all_trees()
+
+
+# for i, j in hashmap_tree.hashmap.items():
+#     print(j.search([int(digit) for digit in str(452749)]))
+#     # hashmap_tree.visualize_tree(i)
+
+
+
+# print()
+        
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    hashmap_tree = HashMapTree()
+
+    for _ in range(1000):
+        key = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+        hashmap_tree.insert_empty_tree(key)
+
+    # For each key, generate between 1000-10000 random numbers and insert them into the corresponding tree
+        
+    # for key in hashmap_tree.hashmap.keys():
+    #     num_elements = random.randint(1000, 10000)
+    #     random_numbers = generate_random_numbers(100000, 999999, num_elements)
+    #     for number in random_numbers:
+    #         hashmap_tree.insert_digit(key, number)
+    for key in hashmap_tree.hashmap.keys():
+        num_elements = random.randint(100, 200)
+        random_numbers = generate_random_numbers(100000, 999999, num_elements)
+        # for number in random_numbers:
+        t = threading.Thread(target=insert_digit_wrapper, args=(hashmap_tree, key, random_numbers))
+        t.start()
+
+    # hashmap_tree.visualize()
+
+    # for i, j in hashmap_tree.hashmap.items():
+    #     print(j.search([int(digit) for digit in str(452749)]))
+        # hashmap_tree.visualize_tree(i)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print("Elapsed Time: {} seconds".format(elapsed_time))
