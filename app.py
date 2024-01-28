31b0aae4d1cb3c86b25b5251c25d38e1d59bd3a4
mukul-kr <mukul0000kumar@gmail.com>
2024-01-28 18:51:00 +0530

    tree changes

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ import time
 import pickle
 import threading
 import datetime
+from typing import List
 import uuid
 import logging
 import json
 
 import uuid
 import hashlib
+from collections import deque
+
 
 # logging.basicConfig(filename=f'info-{datetime.datetime.now()}.log', encoding='utf-8', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.basicConfig( encoding='utf-8', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -18,14 +21,18 @@ logging.basicConfig( encoding='utf-8', level=logging.INFO, format='%(asctime)s -
 class TreeNode:
     def __init__(self, digit):
         self.digit = digit
-        self.children = set()
+        self.children = []
 
     def to_dict(self):
         return {'d': self.digit, 'c': [child.to_dict() for child in self.children]}
 
+
+
+
 class BinaryTree:
     def __init__(self):
         self.root = None
+        self._string = ""
 
     def to_dict(self):
         return self.root.to_dict() if self.root else None
@@ -44,13 +51,16 @@ class BinaryTree:
         if idx == 0 and node is not None and node.digit == digit_list[idx]:
             return self._insert(node, digit_list, idx + 1)
         if node is None:
-            newchild = TreeNode(digit_list[idx])
+            newchild = TreeNode(-1)
             self.root = newchild
+            return self._insert(newchild, digit_list, idx)
+
         else:
             newchild = TreeNode(digit_list[idx])
             # node.children.append(newchild)
-            node.children.add(newchild)
-        return self._insert(newchild, digit_list, idx + 1)
+            node.children.append(newchild)
+            node.children.sort(key=lambda x: x.digit)
+            return self._insert(newchild, digit_list, idx + 1)
 
     def search(self, digit_list):
         return self._search(self.root, digit_list)
@@ -65,6 +75,91 @@ class BinaryTree:
                 if self._search(child, digit_list, idx + 1):
                     return True
         return False
+    
+
+    def bfs(self) -> List[List[List[int]]]:
+        if self.root is None:
+            return [[[]]]
+        queue = deque([(self.root, 1)]) # Add level information to each node
+        list_tree :  List[List[List[int]]] = [[],[],[],[],[],[]]
+        while len(queue) > 0:
+            node, level = queue.popleft() # Get the node and its level
+            children = [] # Temporary list to store children
+            if node != self.root: # Check if the node is not the root
+                print(f"Node: {node.digit}, Level: {level}") # Process the node and its level
+            for child in node.children:
+                queue.append((child, level + 1)) # Enqueue children with their correct level
+                children.append(child.digit) # Store children digits
+            if children: # If there were any children
+                list_tree[level-1].append(children) # type: ignore # Store children digits
+                # print(f"Children: {children}") 
+        for i in list_tree:
+            print(i)
+        return list_tree
+
+
+
+
+
+
+        
+
+    def create_data_to_save(self):
+        bfs_traversal_data = self.bfs()
+
+        string_data = ""
+        for level_i in bfs_traversal_data:
+            for i in level_i:
+                array = [0,0,0,0,0,0,0,0,0,0]
+                for j in i:
+                    array[j] = 1
+                string_data += "".join(str(e) for e in array)
+            # string_data += "\n"
+
+        return string_data
+
+
+def check_if_key_exists_helper(tree_string, element):
+    pehle_kitne_hai = 0
+    exist_krta_hai = False
+    total_number_of_1s = 0
+    for i in range(10):
+        if tree_string[i] == str(1):
+            if not exist_krta_hai:
+                pehle_kitne_hai += 1
+            total_number_of_1s += 1
+        if i == element:
+            exist_krta_hai = True
+    if exist_krta_hai:
+        return pehle_kitne_hai, total_number_of_1s
+    else:
+        return 0, 0
+
+
+def check_if_key_exists(tree_string ,key):
+    list_of_strings = chunks(tree_string, 10)
+    list_of_digits = [int(i) for i in str(key)]
+
+    pehle_kitne_hai = 0
+    prev = 0
+    total = 0
+    purana_total_no_of_1s = 0
+    total_no_of_1s = 0
+    for i in range(6):
+        print(f"index: {i} starting pehle_kitne_hai: {pehle_kitne_hai}, total_no_of_1s: {total_no_of_1s}, prev: {prev}, purana_total_no_of_1s: {purana_total_no_of_1s} string: {list_of_strings[prev]}")
+
+        pehle_kitne_hai, total_no_of_1s = check_if_key_exists_helper(list_of_strings[prev], list_of_digits[i])
+        prev += pehle_kitne_hai + purana_total_no_of_1s
+        purana_total_no_of_1s = total_no_of_1s
+
+
+        print(f"index: {i} ending pehle_kitne_hai: {pehle_kitne_hai}, total_no_of_1s: {total_no_of_1s}, prev: {prev}, purana_total_no_of_1s: {purana_total_no_of_1s} string: {list_of_strings[prev]}")
+
+
+def chunks(s, n):
+    """Produce `n`-character chunks from `s`."""
+    return [s[i:i+n] for i in range(0, len(s), n)]
+
 
 class HashMapTree:
     def __init__(self):
@@ -119,7 +214,7 @@ class HashMapTree:
 
     def _print_tree(self, node, depth):
         if node is not None:
-            logging.info(" " * depth + str(node.digit))
+            logging.info("\t" * depth + str(node.digit))
             for child in node.children:
                 self._print_tree(child, depth + 1)
 
@@ -180,31 +275,34 @@ if __name__ == "__main__":
     start_time = time.time()
     hashmap_tree = HashMapTree()
 
-    # hashmap_tree.insert_empty_tree("0")
+    hashmap_tree.insert_empty_tree("0")
     # hashmap_tree.insert_empty_tree("1")
 
-    # # Insert digits into trees
-    # hashmap_tree.insert_digit("0", 456789)
-    # hashmap_tree.insert_digit("0", 452749)
-    # hashmap_tree.insert_digit("0", 452741)
-    # hashmap_tree.insert_digit("0", 466789)
+    # Insert digits into trees
+    hashmap_tree.insert_digit("0", 411111)
+    hashmap_tree.insert_digit("0", 456789)
+    hashmap_tree.insert_digit("0", 452749)
+    hashmap_tree.insert_digit("0", 452741)
+    hashmap_tree.insert_digit("0", 466789)
+    hashmap_tree.insert_digit("0", 488888)
+    hashmap_tree.insert_digit("0", 566789)
     # hashmap_tree.insert_digit("1", 456789)
     # hashmap_tree.insert_digit("1", 452789)
 
-    for _ in range(10):
-        logging.info(_)
-        generate_seller_id(hashmap_tree)
+    # for _ in range(1):
+    #     logging.info(_)
+    #     generate_seller_id(hashmap_tree)
 
 
-    # # hashmap_tree.print_all_trees()
+    hashmap_tree.print_all_trees()
 
     # # For each key, generate between 1000-10000 random numbers and insert them into the corresponding tree
         
-    for key in hashmap_tree.hashmap.keys():
-        num_elements = random.randint(1000, 10000)
-        random_numbers = generate_random_numbers(100000, 999999, num_elements)
-        for number in random_numbers:
-            hashmap_tree.insert_digit(key, number)
+    # for key in hashmap_tree.hashmap.keys():
+    #     num_elements = random.randint(100, 100)
+    #     random_numbers = generate_random_numbers(100000, 999999, num_elements)
+    #     for number in random_numbers:
+    #         hashmap_tree.insert_digit(key, number)
     # # for key in hashmap_tree.hashmap.keys():
     # #     logging.info(f"inserting in {key}")
     # #     num_elements = random.randint(1000, 10000)
@@ -212,15 +310,25 @@ if __name__ == "__main__":
     # #     # for number in random_numbers:
     # #     t = threading.Thread(target=insert_digit_wrapper, args=(hashmap_tree, key, random_numbers))
     # #     t.start()
+            
+    for tree in hashmap_tree.hashmap.values():
+        # tree.bfs()
+        data = tree.create_data_to_save()
+        for i in range(len(chunks(data, 10))):
+
+            print(f"({i} : {chunks(data, 10)[i]})", end=" ")
+
+        print()
+        check_if_key_exists(data, 456789)
     
 
-    out_file = open("myfile.json", "w")
+    # out_file = open("myfile.txt", "w")
 
-    # Dump the Python object into the file
-    json.dump(hashmap_tree.to_dict(), out_file)
+    # # Dump the Python object into the file
+    # # json.dump(hashmap_tree.to_dict(), out_file)
 
-    # Close the file
-    out_file.close()
+    # # Close the file
+    # out_file.close()
 
     end_time = time.time()
     # with open('my_file.pickle', 'wb') as file:
